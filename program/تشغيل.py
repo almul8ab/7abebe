258104ac45f932f31c7cf1747d4b42d51886ec47
Jepthon Arabic <91113247+almul8ab@users.noreply.github.com>
2022-05-25 15:50:49 +0300

    Update تشغيل.py

--- program/تشغيل.py
+++ program/تشغيل.py
@@ -1,10 +1,11 @@
-import asyncio
 import re
+import asyncio
 import requests
-from config import ASSISTANT_NAME, IMG_1, IMG_2
-from driver.filters import command2, other_filters
+from config import ASSISTANT_NAME, BOT_USERNAME, IMG_1, IMG_2
+from driver.filters import command, other_filters
 from driver.queues import QUEUE, add_to_queue
 from driver.veez import call_py, user
+from driver.utils import bash
 from pyrogram import Client
 from pyrogram.errors import UserAlreadyParticipant, UserNotParticipant
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
